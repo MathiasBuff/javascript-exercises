4755b8cff0269af9828e33916f3c5dbed0d0853e
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-03 17:56:31 +0200

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let output = [];
+    for (element of arr) {
+        if (args.includes(element)) {
+            continue;
+        } else {
+            output.push(element);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
