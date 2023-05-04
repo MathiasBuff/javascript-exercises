9551fffa9e86a3f0d34b6e1536a309a4077df2fa
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-04 14:31:46 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x1, x2) {
+    // Check that the arguments are within constraints
+    if (typeof(x1) != "number" || typeof(x2) != "number" || x1 < 0 || x2 < 0) {
+        return "ERROR";
+    }
 
+    const max = Math.max(x1, x2);
+    const min = Math.min(x1, x2);
+    
+    // O(1) algorithm with math
+    // return (max * max - max * min + 2 * max - min + 1) / 2;
+
+    // O(n) algorithm with loop, n = max-min
+    let sum = 0;
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
