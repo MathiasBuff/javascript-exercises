9c7c9fc93e3af7cea8dc834b0e6f369f99dd34c9
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-04 14:43:52 +0200

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
 
+    return !(year % 400)? true : !(year % 100)? false : !(year % 4)? true : false;
+    // Equivalent to:
+    // if (year % 400 == 0) {
+    //     return true;
+    // } else if (year % 100 == 0) {
+    //     return false;
+    // } else if (year % 4 == 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
 
 // Do not edit below this line
