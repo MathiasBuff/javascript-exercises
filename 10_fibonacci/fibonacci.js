e09ca95d33860814cfa84e213291afb05ff09a4a
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-10 10:49:31 +0200

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // handle string input
+    if (typeof(n) == "string") n = Number(n);
+    
+    // check for edge cases
+    if (n < 0) return "OOPS";
+    if (n < 3) return 1;
+    
+    let old = 1, cur = 1, tmp = 1;
+    for (i = 2; i < n; i++) {
+        tmp = cur;
+        cur = tmp + old;
+        old = tmp;
+    }
+    return cur
 };
 
 // Do not edit below this line
