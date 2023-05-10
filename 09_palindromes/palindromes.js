6aa4bcfefcb42659c35b22f873bcbbf7db11e551
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-10 10:41:17 +0200

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^0-9A-Z]+/gi,"");
+    return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
